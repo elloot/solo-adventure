935212c7c177eac055738827ff7b783e8fab4ad9
elloot <elliot.duchek@gmail.com>
2020-10-16 10:39:25 +0200

    move database queries to new router game

---
+++ routes/game.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const { query } = require('../models/db');
+
+router.get('/:id', async function (req, res, next) {
+  try {
+    const story = await query(
+      'SELECT * FROM story WHERE id = ?',
+      req.params.id
+    );
+
+    const links = await query(
+      'SELECT * FROM links WHERE story_id = ?',
+      req.params.id
+    );
+
+    res.render('test', {
+      id: req.params.id,
+      story: story,
+      links: links
+    });
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
+module.exports = router;
