848b7375f78a195a99f2d318ec6a5aa0e982ec32
elloot <elliot.duchek@gmail.com>
2020-10-23 12:56:15 +0200

    animate links and story when a link is pressed, redirect when animation is finished

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,5 +4,16 @@ const links = document.querySelectorAll('.link');
 links.forEach(link => {
   link.addEventListener('click', (e) => {
     e.preventDefault();
+    const clickedLink = e.currentTarget;
+    const url = clickedLink.href;
+
+    story.classList.add('flying-out');
+    for (let i = 0; i < links.length; i++) {
+      links[i].classList.add('flying-out');
+    }
+
+    clickedLink.addEventListener('animationend', (e) => {
+      window.location.href = url;
+    });
   });
 });
