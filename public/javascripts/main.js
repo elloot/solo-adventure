080cdcab7eea59934c4e5746e1aba3bb4ac3790d
elloot <elliot.duchek@gmail.com>
2020-10-25 17:34:19 +0100

    position sign based on screen height

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,11 @@
+// make the signs base connect to the bottom of the screen
+const sign = document.querySelector('.sign-board');
+const signTop = sign.getBoundingClientRect().top;
+const signBase = document.querySelector('.sign-stand__base');
+
+signBase.style.height = (window.innerHeight - (signTop + sign.clientHeight)) + 'px';
+
+// animates sign-board contents when a link is pressed
 const story = document.querySelector('.story-container');
 const links = document.querySelectorAll('.link');
 
